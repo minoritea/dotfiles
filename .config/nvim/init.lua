d7452a6c49c61946e8c8d8f17d265a2fbc372ead
Minori Tokuda <snowcrush@mail.0pt.jp>
2025-12-07 11:58:32 +0900

    use default LSP instead of coc

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -6,24 +6,20 @@ vim.o.number = true
 vim.o.mouse = ""
 vim.o.shell = '/usr/bin/zsh'
 vim.g.clipboard = 'osc52'
-vim.o.undodir = vim.fn.expand('~/.cache/nvim/undo')
 vim.o.undofile = true
 vim.opt.clipboard:append({"unnamedplus"})
 
 require('config.lazy')
 require('lazy').setup({
+  dev = {
+    path = '~/p/neovim',
+  },
   spec = {
-    -- LSP
-    { 'neoclide/coc.nvim', branch = 'release', },
-
     -- Filer
-    { 'stevearc/oil.nvim', priority = 1, config = function()
-        require('oil').setup({ default_file_explorer = true, view_options = { show_hidden = true, }, })
-        vim.api.nvim_create_user_command('OilP', function()
-          require('oil').open(vim.fn.expand('%:p:h'))
-        end, {})
-      end,
-    },
+    { 'stevearc/oil.nvim', priority = 1, opts = {
+      default_file_explorer = true,
+      view_options = { show_hidden = true, },
+    } },
     { 'ibhagwan/fzf-lua', opts = {}, },
 
     -- Syntax Highlighting
@@ -55,9 +51,32 @@ local ts_langs = {
   'sql',
 }
 
+-- LSP
+vim.lsp.config['go'] = {
+  cmd = { 'gopls', 'serve' },
+  filetypes = { 'go', 'gomod' },
+  root_markers = { 'go.mod', '.git' },
+}
+vim.lsp.config['ts'] = {
+  cmd = { vim.fn.expand('~/.bun/bin/tsgo'), '--lsp', '--stdio' },
+  filetypes = {
+    'typescript',
+    'typescriptreact',
+    'typescript.tsx',
+    'javascript',
+    'javascriptreact',
+    'javascript.jsx',
+  },
+  root_markers = { 'package.json', 'tsconfig.json', '.git' },
+}
+vim.lsp.enable({ 'go', 'ts' })
+
 vim.api.nvim_create_user_command('TSInstallAll', function()
-  local ts = require('nvim-treesitter')
-  ts.install(ts_langs)
+  require('nvim-treesitter').install(ts_langs)
+end, {})
+
+vim.api.nvim_create_user_command('TSUpdateAll', function()
+  require('nvim-treesitter').update(ts_langs)
 end, {})
 
 local fileTypeGroup = vim.api.nvim_create_augroup('FileTypeCmds', { clear = true })
@@ -82,18 +101,17 @@ vim.keymap.set('', 'q', '<Nop>')
 vim.keymap.set('', 'q:', '<Nop>')
 vim.keymap.set('', '<Right>', ':bn<CR>')
 vim.keymap.set('', '<Left>', ':bp<CR>')
-vim.keymap.set('', '<C-w>t', '<C-w>T')
 vim.keymap.set('', '<C-p>b', function() fzf_lua.buffers() end)
 vim.keymap.set('', '<C-p>f', function() fzf_lua.files() end)
 vim.keymap.set('', '<C-p>r', function() fzf_lua.live_grep() end)
-vim.keymap.set('', '<C-]>', '<Plug>(coc-definition)')
-vim.keymap.set('', '<C-T>', '<C-O>')
-vim.keymap.set('', '<C-K>', ":call CocAction('doHover')<CR>")
-vim.keymap.set('n', 'gcr', '<Plug>(coc-references-used)')
-vim.keymap.set('n', 'gci', '<Plug>(coc-implementation)')
-vim.keymap.set('n', 'gct', '<Plug>(coc-type-definition)')
-vim.keymap.set('n', 'gcp', '<Plug>(coc-diagnostic-prev)')
-vim.keymap.set('n', 'gcn', '<Plug>(coc-diagnostic-next)')
+-- C-t -> vim.lsp.tagfunc()
+-- grn -> vim.lsp.buf.rename()
+-- gra -> vim.lsp.buf.code_action()
+-- grr -> vim.lsp.buf.references()
+-- gri -> vim.lsp.buf.implementation()
+-- grt -> vim.lsp.buf.type_definition()
+-- g0  -> vim.lsp.buf.document_symbol()
+-- C-s -> vim.lsp.buf.signature_help()
 vim.keymap.set('i', '<C-j>j', '<Plug>(copilot-dismiss)')
 vim.keymap.set('i', '<C-j><C-j>', '<Plug>(copilot-dismiss)')
 vim.keymap.set('i', '<C-j><C-n>', '<Plug>(copilot-next)')
@@ -105,4 +123,3 @@ vim.keymap.set('i', '<C-j>$', '<Plug>(copilot-accept-line)')
 vim.keymap.set('i', '<C-j><C-$>', '<Plug>(copilot-accept-line)')
 vim.keymap.set('i', '<C-j>s', '<Plug>(copilot-suggest)')
 vim.keymap.set('i', '<C-j><C-s>', '<Plug>(copilot-suggest)')
-vim.keymap.set('i', '<expr><C-w>;', 'coc#pum#close()')
