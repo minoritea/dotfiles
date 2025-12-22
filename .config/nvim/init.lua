0ab11a0162d5ab746e2a1206d4920ae03f447e4b
Minori Tokuda <snowcrush@mail.0pt.jp>
2025-12-22 03:08:55 +0000

    update init.lua

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -9,6 +9,18 @@ vim.g.clipboard = 'osc52'
 vim.o.undofile = true
 vim.opt.clipboard:append({"unnamedplus"})
 
+local function carbon_float_settings()
+  return {
+    relative = 'editor',
+    style = 'minimal',
+    border = 'rounded',
+    width = math.floor(vim.o.columns * 0.8),
+    height = math.floor(vim.o.lines * 0.8),
+    col = math.floor(vim.o.columns * 0.1),
+    row = math.floor(vim.o.lines * 0.1),
+  }
+end
+
 require('config.lazy')
 require('lazy').setup({
   dev = {
@@ -21,6 +33,7 @@ require('lazy').setup({
       view_options = { show_hidden = true, },
     } },
     { 'ibhagwan/fzf-lua', opts = {}, },
+    { 'SidOfc/carbon.nvim', opts = { auto_open = false, auto_reveal = true, exclude = { '%.git$' }, float_settings = carbon_float_settings }, },
 
     -- Syntax Highlighting
     { 'nvim-treesitter/nvim-treesitter', lazy = false, branch = 'main', build = ':TSUpdate', },
@@ -30,6 +43,9 @@ require('lazy').setup({
 
     -- Git
     { 'sindrets/diffview.nvim', opts = {}, },
+
+    -- diff
+    { 'andrewradev/linediff.vim' },
   },
 })
 
@@ -55,10 +71,11 @@ local ts_langs = {
 vim.lsp.config['go'] = {
   cmd = { 'gopls', 'serve' },
   filetypes = { 'go', 'gomod' },
-  root_markers = { 'go.mod', '.git' },
+  root_markers = { 'go.mod' },
 }
 vim.lsp.config['ts'] = {
-  cmd = { vim.fn.expand('~/.bun/bin/tsgo'), '--lsp', '--stdio' },
+  -- cmd = { vim.fn.expand('~/.bun/bin/tsgo'), '--lsp', '--stdio' },
+  cmd = { vim.fn.expand('~/.bun/bin/vtsls'), '--stdio' },
   filetypes = {
     'typescript',
     'typescriptreact',
@@ -67,30 +84,28 @@ vim.lsp.config['ts'] = {
     'javascriptreact',
     'javascript.jsx',
   },
-  root_markers = { 'package.json', 'tsconfig.json', '.git' },
+  root_markers = { 'package.json', 'tsconfig.json' },
 }
 vim.lsp.enable({ 'go', 'ts' })
 
-vim.api.nvim_create_user_command('TSInstallAll', function()
-  require('nvim-treesitter').install(ts_langs)
-end, {})
-
-vim.api.nvim_create_user_command('TSUpdateAll', function()
-  require('nvim-treesitter').update(ts_langs)
-end, {})
-
-local fileTypeGroup = vim.api.nvim_create_augroup('FileTypeCmds', { clear = true })
+local custom_cmd_group = vim.api.nvim_create_augroup('CustomCommands', { clear = true })
 
 vim.api.nvim_create_autocmd('FileType', {
-  pattern = ts_langs,
-  group = fileTypeGroup,
-  callback = function() vim.treesitter.start() end,
+  pattern = "*",
+  group = custom_cmd_group,
+  callback = function() pcall(vim.treesitter.start) end,
 })
 
 vim.api.nvim_create_autocmd('FileType', {
   pattern = { 'go' },
-  group = fileTypeGroup,
+  group = custom_cmd_group,
   callback = function() vim.bo.expandtab = false end,
+})
+
+vim.api.nvim_create_autocmd('BufWritePre', {
+  pattern = '*',
+  group = custom_cmd_group,
+  callback = vim.lsp.buf.format,
 })
 
 local fzf_lua = require('fzf-lua')
@@ -101,16 +116,17 @@ vim.keymap.set('', 'q', '<Nop>')
 vim.keymap.set('', 'q:', '<Nop>')
 vim.keymap.set('', '<Right>', ':bn<CR>')
 vim.keymap.set('', '<Left>', ':bp<CR>')
-vim.keymap.set('', '<C-p>b', function() fzf_lua.buffers() end)
-vim.keymap.set('', '<C-p>f', function() fzf_lua.files() end)
-vim.keymap.set('', '<C-p>r', function() fzf_lua.live_grep() end)
+vim.keymap.set('', '<C-p>b', fzf_lua.buffers)
+vim.keymap.set('', '<C-p>f', fzf_lua.files)
+vim.keymap.set('', '<C-p>r', fzf_lua.live_grep)
+vim.keymap.set('', '<C-n>', require('carbon').explore_float)
 -- C-t -> vim.lsp.tagfunc()
 -- grn -> vim.lsp.buf.rename()
--- gra -> vim.lsp.buf.code_action()
--- grr -> vim.lsp.buf.references()
--- gri -> vim.lsp.buf.implementation()
--- grt -> vim.lsp.buf.type_definition()
--- g0  -> vim.lsp.buf.document_symbol()
+vim.keymap.set('v', 'gra', fzf_lua.lsp_code_actions)
+vim.keymap.set('n', 'grr', fzf_lua.lsp_references)
+vim.keymap.set('n', 'gri', fzf_lua.lsp_implementations)
+vim.keymap.set('n', 'grt', fzf_lua.lsp_typedefs)
+vim.keymap.set('n', 'g0', fzf_lua.lsp_document_symbols)
 -- C-s -> vim.lsp.buf.signature_help()
 vim.keymap.set('i', '<C-j>j', '<Plug>(copilot-dismiss)')
 vim.keymap.set('i', '<C-j><C-j>', '<Plug>(copilot-dismiss)')
